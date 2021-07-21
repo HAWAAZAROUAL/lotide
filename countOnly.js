458f712fdc977681030b1db091e0a2c45f0a4ff2
Hawaa <hawaazaroual@gmail.com>
2021-07-21 16:08:29 -0600

    All assertions passed

---
+++ countOnly.js
@@ -0,0 +1,76 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+/*
+const countOnly = function(allItems, itemsToCount) {
+  let instanceCount = [];
+  for (i = 0; i < allItems.length; i++){
+    //In here I now want to count the instances of allItems[i]
+  }
+  instanceCount = allItems.length
+
+}
+*/
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]){
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+   }
+  }
+  return results;
+}
+
+//Step 1 - make a function
+// step 2 - go through all the items in the array
+// step 3 - make a counter of how many times each item occurs
+// step 4 - push/ place/ move these results into a variablef # of instances of the items in the array
+//Step 2 - they need to be returned as an object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
