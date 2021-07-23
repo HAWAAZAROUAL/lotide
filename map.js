82fdc944b1fe3ec984efa07d7f4eea4738f188ec
Hawaa <hawaazaroual@gmail.com>
2021-07-22 18:33:17 -0600

    pushing map.js

---
+++ map.js
@@ -0,0 +1,16 @@
+
+const words = ["hello", "darkness", "my", "old", "friend"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+let results5 = map(words, word => word.length)
+console.log(results5);
