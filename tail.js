25689ba5a9b51dbfb2332c9f3ebf6f9a7acdc59f
Hawaa <hawaazaroual@gmail.com>
2021-07-26 15:24:51 -0600

    refactored tail to export into the test file

--- tail.js
+++ tail.js
@@ -6,16 +6,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// const assertEqual = require('./assertEqual');
+
 const tail = function(arr) {
   let array = [];
-  for (let i = 1; i < arr.length; i++) { //This starts the looping from the second element in array- therefore excluding the first.
-    array.push(arr[i]); // Now we are pushing the elements from the loop into this new variable to create and array.
+  for (let i = 1; i < arr.length; i++) {
+    array.push(arr[i]); 
   }
   return array;
 };
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
